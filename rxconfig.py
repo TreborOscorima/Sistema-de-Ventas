e692c6864901648b457941da81b709811bf6b5b3
Trebor Oscorima <tu-email@ejemplo.com>
2025-12-05 15:14:51 -0300

    Cambios en el archivo de configuracion para uso local

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,14 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
+    
+    # Conexión a Base de Datos (ESTA ESTÁ PERFECTA, NO LA TOQUES)
     db_url="mysql+pymysql://root:TreborOD%28523%29@localhost:3306/sistema_ventas",
+    
+    # --- AGREGA ESTA LÍNEA AQUÍ ---
+    api_url="http://localhost:8000",
+    # ------------------------------
+
     plugins=[rx.plugins.TailwindV3Plugin()],
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
     telemetry_enabled=True,
